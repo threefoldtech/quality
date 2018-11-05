22d15469f5fe6e2b9f2ae03ad46b088c10742124
Maxime Daniel <maxux.unix@gmail.com>
2018-11-05 12:54:37 +0100

    Update flist.sh

--- flist/flist.sh
+++ flist/flist.sh
@@ -1,53 +1,67 @@
 #!/bin/bash
-# This flist has : 
+# This flist has :
 #     ubuntu 16.04, js(development), most of test suite requirements library, zerotier
 
 set -ex
 
 # make output directory
 mkdir -p /tmp/archives
+rootdir="/tmp/xtremx"
 
-mkdir -p /tmp/xTremX/proc
-mkdir -p /tmp/xTremX/sys
-mkdir -p /tmp/xTremX/dev
-mkdir -p /tmp/xTremX/etc/apt
-mkdir -p /tmp/xTremX/etc/initramfs-tools
-mkdir -p /tmp/xTremX/etc/network/interfaces.d
+mkdir -p ${rootdir}/proc
+mkdir -p ${rootdir}/sys
+mkdir -p ${rootdir}/dev
+mkdir -p ${rootdir}/etc/apt
+mkdir -p ${rootdir}/etc/initramfs-tools
+mkdir -p ${rootdir}/etc/network/interfaces.d
 
-mount -o bind /proc /tmp/xTremX/proc
-mount -o bind /sys /tmp/xTremX/sys
-mount -o bind /dev /tmp/xTremX/dev
+apt-get update
+apt-get install -y debootstrap # basic debian system
+debootstrap xenial ${rootdir} http://ftp.belnet.be/ubuntu.com/ubuntu
 
-apt-get install -y debootstrap #basic debian system
-debootstrap xenial /tmp/xTremX http://ftp.belnet.be/ubuntu.com/ubuntu
+cat > ${rootdir}/etc/apt/sources.list << EOF
+deb http://archive.ubuntu.com/ubuntu/ xenial main universe multiverse restricted
+deb http://download.zerotier.com/debian/xenial xenial main
+EOF
 
-printf "deb http://archive.ubuntu.com/ubuntu/ xenial main universe multiverse restricted\n" >> /tmp/xTremX/etc/apt/sources.list
-printf "deb http://download.zerotier.com/debian/xenial xenial main\n" >> /tmp/xTremX/etc/apt/sources.list
+cat >> ${rootdir}/etc/locale.gen << EOF
+en_US.UTF-8 UTF-8
+en_GB.UTF-8 UTF-8
+EOF
 
-printf "en_US.UTF-8 UTF-8\n" >> /tmp/xTremX/etc/locale.gen
-printf "en_GB.UTF-8 UTF-8\n" >> /tmp/xTremX/etc/locale.gen
+cat > ${rootdir}/etc/initramfs-tools/modules << EOF
+9p
+9pnet
+9pnet_virtio
+9pnet_rdma
+EOF
 
-printf "9p\n9pnet\n9pnet_virtio\n9pnet_rdma\n" > /tmp/xTremX/etc/initramfs-tools/modules
-printf 'root    /    9p    rw,cache=loose,trans=virtio 0 0\n' > /tmp/xTremX/etc/fstab
-printf 'auto ens4\niface ens4 inet dhcp\n' >> /tmp/xTremX/etc/network/interfaces.d/ens4
-printf 'ubuntu_xTremX\n' > /tmp/xTremX/etc/hostname
-printf 'nameserver 8.8.8.8\n' > /tmp/xTremX/etc/resolv.conf
+cat > ${rootdir}/etc/network/interfaces.d/ens4 << EOF
+auto ens4
+iface ens4 inet dhcp
+EOF
 
-chroot /tmp/xTremX bash -c 'locale-gen'
-chroot /tmp/xTremX bash -c 'apt-get update'
-chroot /tmp/xTremX bash -c 'apt-get install -y --allow-unauthenticated --no-install-recommends openssh-server linux-generic wget ca-certificates curl acpid'
-chroot /tmp/xTremX bash -c 'update-initramfs -u'
-#chroot /tmp/xTremX bash -c 'curl -s https://install.zerotier.com/ | bash'
+echo "root    /    9p    rw,cache=loose,trans=virtio 0 0" > ${rootdir}/etc/fstab
+echo "ubuntu_xTremX" > ${rootdir}/etc/hostname
+echo "nameserver 8.8.8.8" > ${rootdir}/etc/resolv.conf
 
-printf "kernel: /boot/vmlinuz-4.4.0-21-generic\n" > /tmp/xTremX/boot/boot.yaml
-printf "initrd: /boot/initrd.img-4.4.0-21-generic\n" >> /tmp/xTremX/boot/boot.yaml
+mount -o bind /proc ${rootdir}/proc
 
-chroot /tmp/xTremX bash -c 'printf "root:root" | chpasswd'
+chroot ${rootdir} bash -c 'locale-gen'
+chroot ${rootdir} bash -c 'apt-get update'
+chroot ${rootdir} bash -c 'apt-get install -y --allow-unauthenticated --no-install-recommends openssh-server linux-generic wget ca-certificates curl acpid'
+chroot ${rootdir} bash -c 'update-initramfs -u'
+#chroot ${rootdir} bash -c 'curl -s https://install.zerotier.com/ | bash'
 
-umount /tmp/xTremX/proc
-umount /tmp/xTremX/sys
-umount /tmp/xTremX/dev
+cat > ${rootdir}/boot/boot.yaml << EOF
+kernel: /boot/vmlinuz-4.4.0-21-generic
+initrd: /boot/initrd.img-4.4.0-21-generic
+EOF
 
-rm -rf /tmp/xTremX/var/apt/cache/archives
+chroot ${rootdir} bash -c 'echo "root:root" | chpasswd'
 
-tar -czpf "/tmp/archives/testing_flist.tar.gz" --exclude tmp --exclude dev --exclude sys --exclude proc -C /tmp/xTremX .
+umount ${rootdir}/proc
+
+rm -rf ${rootdir}/var/apt/cache/archives
+
+tar -czpf "/tmp/archives/testing_flist.tar.gz" --exclude mnt --exclude tmp --exclude dev --exclude sys --exclude proc -C ${rootdir} .
